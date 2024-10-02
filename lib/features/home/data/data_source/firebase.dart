5fd07db990d1300881826475d83fc623b9686b39
Mohd Jasir Khan <khanmohdjasir@gmail.com>
2024-10-02 10:05:16 +0530

    fix: added ss to readme

--- lib/features/home/data/data_source/firebase.dart
+++ lib/features/home/data/data_source/firebase.dart
@@ -5,11 +5,15 @@ class FirseBaseDataSource {
   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
 
   Future<List<PersonDataModel>> getHomeData() async {
-    final QuerySnapshot<Map<String, dynamic>> querySnapshot =
+    try {
+       final QuerySnapshot<Map<String, dynamic>> querySnapshot =
         await _firestore.collection('person').get();
     return querySnapshot.docs
         .map((QueryDocumentSnapshot<Map<String, dynamic>> e) =>
             PersonDataModel.fromJson(e.data()))
         .toList();
+    } catch(e) {
+      throw Exception(e);
+    }
   }
 }
